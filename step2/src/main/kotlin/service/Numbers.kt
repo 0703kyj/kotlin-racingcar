ab2524f0a5a9377433c10b15781eb6d9cce8ae42
0703kyj <kyj0703@konkuk.ac.kr>
2024-11-13 22:10:04 +0900

    [STEP-2] test: NumbersTest 추가

--- step2/src/main/kotlin/service/Numbers.kt
+++ step2/src/main/kotlin/service/Numbers.kt
@@ -8,20 +8,22 @@ data class Numbers(
 
     val firstNumber
         get() = values.getOrElse(0) {
-            throw IllegalStateException()
+            throw IllegalStateException("[Numbers] 첫 번째 숫자가 존재하지 않습니다. | size: $size")
         }
 
     val secondNumber
         get() = values.getOrElse(1) {
-            throw IllegalStateException()
+            throw IllegalStateException("[Numbers] 두 번째 숫자가 존재하지 않습니다. | size: $size")
         }
 
     fun getResult(): Double {
-        check(values.size == 1)
+        check(size == 1) {
+            "[Numbers] getResult 는 size 가 1일 때 사용할 수 있습니다. | size: $size"
+        }
         return values[0]
     }
 
-    fun canCalculate() = values.size >= 2
+    fun canCalculate() = size >= 2
 
     fun updateAfterCalculate(result: Double) {
         values = listOf(result) + values.drop(2)
