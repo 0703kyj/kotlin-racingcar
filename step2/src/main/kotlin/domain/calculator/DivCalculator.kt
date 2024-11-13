359146b6bb371e7a2aee64fbbfbb038a3b13a7c3
0703kyj <kyj0703@konkuk.ac.kr>
2024-11-13 22:24:07 +0900

    [STEP-2] feature: 0으로 나누는 예외 케이스 처리

--- step2/src/main/kotlin/domain/calculator/DivCalculator.kt
+++ step2/src/main/kotlin/domain/calculator/DivCalculator.kt
@@ -9,6 +9,12 @@ class DivCalculator : Calculator {
         firstNumber: Double,
         secondNumber: Double,
     ): Double {
+        require(secondNumber.isNotZero()) {
+            "[DivCalculator] 0으로 나눌 수 없습니다. | firstNumber: $firstNumber, secondNumber: $secondNumber"
+        }
+
         return firstNumber / secondNumber
     }
+
+    private fun Double.isNotZero() = this != 0.0
 }
--- step2/src/test/kotlin/domain/calculator/CalculatorTest.kt
+++ step2/src/test/kotlin/domain/calculator/CalculatorTest.kt
@@ -1,7 +1,9 @@
 package domain.calculator
 
 import Fixture.easyRandom
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -36,7 +38,7 @@ class CalculatorTest {
         }
 
         @Test
-        fun `덧셈을 수행한다`() {
+        fun `빼기를 수행한다`() {
             val firstNumber = easyRandom<Double>()
             val secondNumber = easyRandom<Double>()
 
@@ -54,7 +56,7 @@ class CalculatorTest {
         }
 
         @Test
-        fun `덧셈을 수행한다`() {
+        fun `곱셈을 수행한다`() {
             val firstNumber = easyRandom<Double>()
             val secondNumber = easyRandom<Double>()
 
@@ -79,6 +81,18 @@ class CalculatorTest {
             val result = calculator.calculate(firstNumber, secondNumber)
 
             result shouldBe firstNumber / secondNumber
+        }
+
+        @Test
+        fun `0으로 나눌 수 없다`() {
+            val firstNumber = easyRandom<Double>()
+            val secondNumber = 0.0
+
+            val exception = shouldThrowExactly<IllegalArgumentException> {
+                calculator.calculate(firstNumber, secondNumber)
+            }
+
+            exception.message shouldContain "0으로 나눌 수 없습니다"
         }
     }
 }
