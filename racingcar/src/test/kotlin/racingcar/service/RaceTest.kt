61c0ad46b62371e1e1191c5b06f06a17af12b6eb
0703kyj <kyj0703@konkuk.ac.kr>
2024-11-18 22:50:49 +0900

    [STEP-4] race test 추가

--- racingcar/src/test/kotlin/racingcar/service/RaceTest.kt
+++ racingcar/src/test/kotlin/racingcar/service/RaceTest.kt
@@ -4,6 +4,7 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import racingcar.RaceConfig
+import racingcar.domain.car.Car
 import racingcar.domain.car.CarRepository
 import racingcar.domain.car.CarService
 
@@ -25,5 +26,33 @@ class RaceTest {
 
         val cars = CarRepository.findAll()
         cars.any { it.currentPosition != 0 } shouldBe true
+    }
+
+    @Test
+    fun `우승자는 position이 가장 큰 Car로 선정된다`() {
+        List(10) { Car(name = "name$it", position = it) }.forEach {
+            CarRepository.save(it)
+        }
+
+        val findWinners = race.findWinners()
+        val winner = findWinners.first()
+
+        findWinners.size shouldBe 1
+        winner.currentPosition shouldBe 9
+        winner.name shouldBe "name9"
+    }
+
+    @Test
+    fun `공동 우승자가 있는 경우 모두 선정된다`() {
+        List(10) { Car(name = "name$it", position = it % 2) }.forEach {
+            CarRepository.save(it)
+        }
+
+        val findWinners = race.findWinners()
+
+        findWinners.size shouldBe 5
+        findWinners.forEach {
+            it.currentPosition shouldBe 1
+        }
     }
 }
